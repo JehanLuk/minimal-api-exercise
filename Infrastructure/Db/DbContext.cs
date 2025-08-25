fadd2e5456466bce1fd0a8131d7d4ddcc42c31b2
Jean <lo856415@gmail.com>
2025-08-25 20:21:47 -0300

    Mudanças 0.2

--- Infrastructure/Db/DbContext.cs
+++ Infrastructure/Db/DbContext.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Domain.Entities;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 namespace MinimalAPI.Infrastructure.Db;
 
-public class DbContext
+public class AppDbContext : DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Administrator> Administrators { get; set; } = default!;
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 using MinimalAPI.DTOs;
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Infrastructure.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContextPool<AppDbContext>(options => {
+    var connString = builder.Configuration.GetConnectionString("psql");
+    options.UseNpgsql(connString);
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
